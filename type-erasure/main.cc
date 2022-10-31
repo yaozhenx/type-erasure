542fbbd6dc5cf45e96192d708abd608242806a20
Zhen Yao <allen.yao@gmail.com>
2022-10-30 22:14:50 -0700

    Minor polish.

--- type-erasure/main.cc
+++ type-erasure/main.cc
@@ -14,12 +14,12 @@ int main() {
   shapes.emplace_back(Square{1.5});
   shapes.emplace_back(Circle{4.2});
 
-  std::cout << "\ndrawing all shapes:\n";
+  std::cout << "\nDrawing all shapes:\n";
   std::for_each(shapes.begin(), shapes.end(), [](const Shape& shape) {
     draw(shape);
   });
 
-  std::cout << "\nserializing all shapes:\n";
+  std::cout << "\nSerializing all shapes:\n";
   std::for_each(shapes.begin(), shapes.end(), [](const Shape& shape) {
     serialize(shape);
   });
